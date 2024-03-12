489561d8e6d885c73f548a16777b59f7380f91c1
Sebastian Achim Mueller <sebastian-achim.mueller@mpi-hd.mpg.de>
2024-03-12 12:55:47 +0100

    use spherical_coordinate's random module

--- spherical_histogram/tests/test_histogram.py
+++ spherical_histogram/tests/test_histogram.py
@@ -3,6 +3,27 @@ import numpy as np
 import spherical_coordinates as sc
 
 
+def draw_az_zd(prng, size=None):
+    return sc.random.uniform_az_zd_in_cone(
+        prng=prng,
+        azimuth_rad=0.0,
+        zenith_rad=0.0,
+        min_half_angle_rad=0.0,
+        max_half_angle_rad=0.5 * np.pi,
+        size=size,
+    )
+
+
+def draw_cx_cy_cz(prng, size=None):
+    az, zd = draw_az_zd(prng=prng, size=size)
+    return sc.az_zd_to_cx_cy_cz(azimuth_rad=az, zenith_rad=zd)
+
+
+def draw_cx_cy(prng, size=None):
+    cx, cy, _ = draw_cx_cy_cz(prng=prng, size=size)
+    return cx, cy
+
+
 def test_init_and_inputs():
     hemihist = sh.HemisphereHistogram(
         num_vertices=200,
@@ -14,15 +35,15 @@ def test_init_and_inputs():
 
     # test cone
     # ---------
-    az, zd = sc.testing.draw_az_zd(prng=prng)
+    az, zd = draw_az_zd(prng=prng)
     hemihist.assign_cone_azimuth_zenith(
         azimuth_rad=az, zenith_rad=zd, half_angle_rad=ha
     )
 
-    cx, cy = sc.testing.draw_cx_cy(prng=prng)
+    cx, cy = draw_cx_cy(prng=prng)
     hemihist.assign_cone_cx_cy(cx=cx, cy=cy, half_angle_rad=ha)
 
-    cx, cy, cz = sc.testing.draw_cx_cy_cz(prng=prng)
+    cx, cy, cz = draw_cx_cy_cz(prng=prng)
     hemihist.assign_cone_cx_cy_cz(cx=cx, cy=cy, cz=cz, half_angle_rad=ha)
 
     # test regular pointing
@@ -30,24 +51,24 @@ def test_init_and_inputs():
 
     # array-like
     SIZE = 100
-    az, zd = sc.testing.draw_az_zd(prng=prng, size=SIZE)
+    az, zd = draw_az_zd(prng=prng, size=SIZE)
     hemihist.assign_azimuth_zenith(azimuth_rad=az, zenith_rad=zd)
 
-    cx, cy = sc.testing.draw_cx_cy(prng=prng, size=SIZE)
+    cx, cy = draw_cx_cy(prng=prng, size=SIZE)
     hemihist.assign_cx_cy(cx=cx, cy=cy)
 
-    cx, cy, cz = sc.testing.draw_cx_cy_cz(prng=prng, size=SIZE)
+    cx, cy, cz = draw_cx_cy_cz(prng=prng, size=SIZE)
     hemihist.assign_cx_cy_cz(cx=cx, cy=cy, cz=cz)
 
     # scalar like
     SIZE = None
-    az, zd = sc.testing.draw_az_zd(prng=prng, size=SIZE)
+    az, zd = draw_az_zd(prng=prng, size=SIZE)
     hemihist.assign_azimuth_zenith(azimuth_rad=az, zenith_rad=zd)
 
-    cx, cy = sc.testing.draw_cx_cy(prng=prng, size=SIZE)
+    cx, cy = draw_cx_cy(prng=prng, size=SIZE)
     hemihist.assign_cx_cy(cx=cx, cy=cy)
 
-    cx, cy, cz = sc.testing.draw_cx_cy_cz(prng=prng, size=SIZE)
+    cx, cy, cz = draw_cx_cy_cz(prng=prng, size=SIZE)
     hemihist.assign_cx_cy_cz(cx=cx, cy=cy, cz=cz)
 
     # test str
